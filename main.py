cb0b2ccd98fb0031d2af207e240b897c6c73275b
maciekwnuk <33461164+maciekwnuk@users.noreply.github.com>
2019-01-13 15:47:22 +0100

    Add files via upload
    Add first version of GUI

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import find_routes as fr
-import airport_data as apd
-import airlines_data as ald
+import sys
 
-# testowe dane
-airport_weights_dic = dict(zip(list(apd.airport_rated)[3:14], [1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1]))
-airlines_weights_dic = dict(zip(list(ald.airlines_rated)[3:12], [5, 3, 3, 3, 3, 3, 3, 3, 1]))
-source = "Warsaw"
-dest = "Zaragoza"
+from mainwindow import Ui_MainWindow
+from PySide2 import QtWidgets, QtGui, QtCore
 
-# print(fr.rate_sorted_paths(source, dest, airport_weights_dic, airlines_weights_dic))
-print(fr.get_sorted_paths(source, dest, airport_weights_dic, airlines_weights_dic))
-
-
+import sys
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
